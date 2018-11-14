2871e9a1cf106f8e0962ca25013cf1adb62e1866
macchiarulo <40037374+macchiarulo@users.noreply.github.com>
2018-11-13 22:50:51 -0500

    Create SourceCode.php

---
+++ SourceCode.php
@@ -0,0 +1,33 @@
+# MySQL-Webpage
+Display MySQL Data on Webpage
+<?php
+$con=mysqli_connect("localhost","root","","rds");
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT * FROM rds.data");
+
+echo "<table border='1'>
+<tr>
+<th>Ticker</th>
+<th>CUSIP</th>
+<th>Buy</th>
+<th>Sale</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row['Ticker'] . "</td>";
+echo "<td>" . $row['CUSIP'] . "</td>";
+echo "<td>" . $row['Buy'] . "</td>";
+echo "<td>" . $row['Sale'] . "</td>";
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+?>
